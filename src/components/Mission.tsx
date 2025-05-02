0384df8069ff1b48db00b9cc812dfd50b6d67b69
Nils <118344410+Nils64640@users.noreply.github.com>
2025-05-02 16:18:05 +0200

    Mission.tsx

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -47,7 +47,7 @@ const Mission = () => {
       <div 
         className="absolute inset-0 bg-cover bg-center opacity-20 pointer-events-none"
         style={{ 
-          backgroundImage: 'url(https://github.com/Nils64640/photo-humanitaire/blob/main/9.jpg?raw=true)'
+          backgroundImage: 'url(https://i.imgur.com/nrKjz5o.jpeg)'
         }}
       ></div>
       
